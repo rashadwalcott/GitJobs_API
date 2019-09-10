cb682658bf7cd719697dc2b7a7e5a33dbfd0ea7e
Rashad <rashad.walcott15@gmail.com>
2019-09-10 10:25:55 -0400

    Edited Auth

--- gitjobs-api/app/controllers/auth_controller.rb
+++ gitjobs-api/app/controllers/auth_controller.rb
@@ -6,7 +6,7 @@ class AuthController < ApplicationController
     if is_authenticated
       render json: {token: create_token(user.id)}
     else
-      redner json: {errors: ['Wrong username or password', 'Please Try Again']}, status: 422
+      render json: {errors: ['Wrong username or password', 'Please Try Again']}, status: 422
     end
   end
 
